ff9a6e1d01e9e621b758c88527f72872625a9838
jihyunniiii <jita1125@naver.com>
2023-12-18 12:54:49 +0900

    [init] #1 Util 추가

---
+++ app/src/main/java/com/donggukthon/team5/util/UiState.kt
@@ -0,0 +1,8 @@
+package com.donggukthon.team5.util
+
+sealed class UiState<out T> {
+    object Loading : UiState<Nothing>()
+    object Empty : UiState<Nothing>()
+    data class Success<T>(val data: T) : UiState<T>()
+    data class Error(val message: String?) : UiState<Nothing>()
+}
