31d055caebdf1fabdb5308054362326d6651fded
KIM MINJEONG <minjeong030420@gmail.com>
2023-12-19 19:26:53 +0900

    [feat] #4 장식하기 클릭 시 기록 modal 띄우기

--- app/src/main/java/com/donggukthon/team5/presentation/tree/TreeActivity.kt
+++ app/src/main/java/com/donggukthon/team5/presentation/tree/TreeActivity.kt
@@ -3,11 +3,22 @@ package com.donggukthon.team5.presentation.tree
 import android.os.Bundle
 import com.donggukthon.team5.R
 import com.donggukthon.team5.databinding.ActivityTreeBinding
+import com.donggukthon.team5.presentation.tree.record.TreeRecordDialogFragment
 import com.donggukthon.team5.util.binding.BindingActivity
 
 class TreeActivity: BindingActivity<ActivityTreeBinding>(R.layout.activity_tree) {
 
+    private var recordDialog: TreeRecordDialogFragment = TreeRecordDialogFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initSetRecordDialog()
+    }
+
+    private fun initSetRecordDialog() {
+        binding.btnTreeMakeDecor.setOnClickListener {
+            recordDialog.show(supportFragmentManager, "DIALOG")
+        }
     }
 }
---
+++ app/src/main/java/com/donggukthon/team5/presentation/tree/record/TreeRecordDialogFragment.kt
@@ -0,0 +1,14 @@
+package com.donggukthon.team5.presentation.tree.record
+
+import android.os.Bundle
+import android.view.View
+import com.donggukthon.team5.R
+import com.donggukthon.team5.databinding.FragmentTreeRecordDialogBinding
+import com.donggukthon.team5.util.binding.BindingDialogFragment
+
+class TreeRecordDialogFragment: BindingDialogFragment<FragmentTreeRecordDialogBinding>(R.layout.fragment_tree_record_dialog) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
