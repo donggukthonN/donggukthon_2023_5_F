2706554d2465c583f560e886acb6647d07721e94
jihyunniiii <jita1125@naver.com>
2023-12-20 07:26:03 +0900

    [chore] #5 충돌 해결

--- app/src/main/java/com/donggukthon/team5/presentation/tree/TreeActivity.kt
+++ app/src/main/java/com/donggukthon/team5/presentation/tree/TreeActivity.kt
@@ -1,9 +1,11 @@
 package com.donggukthon.team5.presentation.tree
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import com.donggukthon.team5.R
 import com.donggukthon.team5.databinding.ActivityTreeBinding
+import com.donggukthon.team5.presentation.tree.record.TreeRecordDialogHintFragment
 import com.donggukthon.team5.util.binding.BindingActivity
 
 class TreeActivity : BindingActivity<ActivityTreeBinding>(R.layout.activity_tree) {
@@ -26,9 +28,10 @@ class TreeActivity : BindingActivity<ActivityTreeBinding>(R.layout.activity_tree
     }
 
     private fun initSetRecordDialog() {
-        binding.btnTreeMakeDecor.setOnClickListener {
+        binding.btnTreeDecorate.setOnClickListener {
             recordDialog.show(supportFragmentManager, DIALOG_TAG)
-            viewModel.btnVisible.value = false
+            binding.btnTreeDecorate.visibility = View.INVISIBLE
+            binding.tvTreePreviousRecord.visibility = View.INVISIBLE
         }
     }
 
