e00ccbd185dfe6cb37ff1183deaf5088bc896b1c
KIM MINJEONG <minjeong030420@gmail.com>
2023-12-19 17:07:13 +0900

    [add] #4 트리뷰 activity 생성

---
+++ app/src/main/java/com/donggukthon/team5/presentation/tree/TreeActivity.kt
@@ -0,0 +1,13 @@
+package com.donggukthon.team5.presentation.tree
+
+import android.os.Bundle
+import com.donggukthon.team5.R
+import com.donggukthon.team5.databinding.ActivityTreeBinding
+import com.donggukthon.team5.util.binding.BindingActivity
+
+class TreeActivity: BindingActivity<ActivityTreeBinding>(R.layout.activity_tree) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
