e82b3f5f7d80ebe316d5bfa79f6ffb8a6a78b45d
jihyunniiii <jita1125@naver.com>
2023-12-20 10:21:25 +0900

    [feat] #9 트리 화면 연결

--- app/src/main/java/com/donggukthon/team5/presentation/tree/TreeActivity.kt
+++ app/src/main/java/com/donggukthon/team5/presentation/tree/TreeActivity.kt
@@ -1,15 +1,18 @@
 package com.donggukthon.team5.presentation.tree
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.donggukthon.team5.R
 import com.donggukthon.team5.databinding.ActivityTreeBinding
+import com.donggukthon.team5.presentation.addtree.AddTreeActivity
 import com.donggukthon.team5.presentation.tree.record.TreeRecordDialogHintFragment
 import com.donggukthon.team5.util.binding.BindingActivity
 
 class TreeActivity : BindingActivity<ActivityTreeBinding>(R.layout.activity_tree) {
     private val treeViewModel by viewModels<TreeViewModel>()
-    private var recordDialog: TreeRecordDialogHintFragment = TreeRecordDialogHintFragment()
+    private var recordDialog: TreeRecordDialogHintFragment =
+        TreeRecordDialogHintFragment(clickRecordBtn = {}, ::moveToAddTree)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +36,15 @@ class TreeActivity : BindingActivity<ActivityTreeBinding>(R.layout.activity_tree
         }
     }
 
+    private fun moveToAddTree() {
+        Intent(this@TreeActivity, AddTreeActivity::class.java).apply {
+            putExtra(ORNAMENT_ID, treeViewModel.ornamentId.value)
+            startActivity(this)
+        }
+    }
 
     companion object {
         const val DIALOG_TAG = "DIALOG_TAG"
+        const val ORNAMENT_ID = "ornamentId"
     }
 }
--- app/src/main/java/com/donggukthon/team5/presentation/tree/TreeViewModel.kt
+++ app/src/main/java/com/donggukthon/team5/presentation/tree/TreeViewModel.kt
@@ -3,7 +3,8 @@ package com.donggukthon.team5.presentation.tree
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import android.util.Log
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class TreeViewModel : ViewModel() {
     val mockOrnamentOverviewList =
@@ -20,4 +21,7 @@ class TreeViewModel : ViewModel() {
     fun isRecordCheckEnabled() {
         _recordEnabled.value = !recordContent.value.isNullOrEmpty()
     }
+
+    private val _ornamentId = MutableStateFlow<Int>(2)
+    val ornamentId = _ornamentId.asStateFlow()
 }
