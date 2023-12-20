85eaecee2b59f14777de651cbb1fc0ffef779c55
jihyunniiii <jita1125@naver.com>
2023-12-20 15:05:31 +0900

    [feat] #13 확장함수 추가

--- app/src/main/java/com/donggukthon/team5/util/extension/ViewExt.kt
+++ app/src/main/java/com/donggukthon/team5/util/extension/ViewExt.kt
@@ -1,9 +1,15 @@
 package com.donggukthon.team5.util.extension
 
 import android.view.View
+import com.donggukthon.team5.util.binding.setInvisibility
 import com.google.android.material.snackbar.Snackbar
 
 fun View.showSnackbar(message: String, isShort: Boolean = true) {
     val duration = if (isShort) Snackbar.LENGTH_SHORT else Snackbar.LENGTH_LONG
     Snackbar.make(this, message, duration).show()
+}
+
+fun View.setInvisibility(isInvisible: Boolean?) {
+    if (isInvisible == null) return
+    this.visibility = if (isInvisible) View.VISIBLE else View.INVISIBLE
 }
