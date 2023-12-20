ae35357b68a8b672f803794f1c1a7c84b2c29497
KIM MINJEONG <minjeong030420@gmail.com>
2023-12-20 14:14:44 +0900

    [add] #11 이전 기억 뷰 서버통신 data 클래스 생성

---
+++ app/src/main/java/com/donggukthon/team5/data/model/response/ResponseOrnamentList.kt
@@ -0,0 +1,31 @@
+package com.donggukthon.team5.data.model.response
+
+import com.donggukthon.team5.domain.model.GoodMemory
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseOrnamentList (
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: List<Data>
+) {
+    @Serializable
+    data class Data(
+        @SerialName("ornamentId")
+        val ornamentId: Int,
+        @SerialName("isGoodMemory")
+        val isGoodMemory: Boolean,
+        @SerialName("memory")
+        val memory: String
+    )
+
+    fun getOrnamentList() = data.map { data ->
+        GoodMemory(
+            ornamentId = data.ornamentId,
+            isGoodMemory = data.isGoodMemory,
+            memory = data.memory
+        )
+    }
+}
--- app/src/main/java/com/donggukthon/team5/domain/model/GoodMemory.kt
+++ app/src/main/java/com/donggukthon/team5/domain/model/GoodMemory.kt
@@ -1,5 +1,9 @@
 package com.donggukthon.team5.domain.model
 
+import kotlinx.serialization.SerialName
+
 data class GoodMemory (
+    val ornamentId: Int = 0,
+    val isGoodMemory: Boolean = false,
     val memory: String = ""
 )
