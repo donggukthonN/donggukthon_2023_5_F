92de516c9340ef0c0402575d5670263623e076d6
jihyunniiii <jita1125@naver.com>
2023-12-20 10:21:08 +0900

    [feat] #9 트리 상세 뷰 구현

---
+++ app/src/main/java/com/donggukthon/team5/presentation/treedetail/TreeDetailActivity.kt
@@ -0,0 +1,34 @@
+package com.donggukthon.team5.presentation.treedetail
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.donggukthon.team5.R
+import com.donggukthon.team5.databinding.ActivityTreeDetailBinding
+import com.donggukthon.team5.presentation.tree.TreeActivity
+import com.donggukthon.team5.util.binding.BindingActivity
+
+class TreeDetailActivity :
+    BindingActivity<ActivityTreeDetailBinding>(R.layout.activity_tree_detail) {
+    private val treeDetailViewModel by viewModels<TreeDetailViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.viewModel = treeDetailViewModel
+
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.ivTreeDetailReturn.setOnClickListener {
+            moveToTree()
+        }
+    }
+
+    private fun moveToTree() {
+        Intent(this@TreeDetailActivity, TreeActivity::class.java).apply {
+            startActivity(this)
+            finish()
+        }
+    }
+}
