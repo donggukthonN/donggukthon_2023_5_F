8a40909f958ae55beeb8391e6800dc38a1b7b2e8
jihyunniiii <jita1125@naver.com>
2023-12-20 15:06:27 +0900

    [feat] #13 오너먼트 추가 트리 뷰 api 연결

--- app/src/main/java/com/donggukthon/team5/presentation/addtree/AddTreeActivity.kt
+++ app/src/main/java/com/donggukthon/team5/presentation/addtree/AddTreeActivity.kt
@@ -2,15 +2,26 @@ package com.donggukthon.team5.presentation.addtree
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
 import com.donggukthon.team5.R
 import com.donggukthon.team5.databinding.ActivityAddTreeBinding
 import com.donggukthon.team5.presentation.tree.TreeRecordActivity.Companion.ORNAMENT_ID
 import com.donggukthon.team5.presentation.treedetail.TreeDetailActivity
+import com.donggukthon.team5.util.UiState
 import com.donggukthon.team5.util.binding.BindingActivity
+import com.donggukthon.team5.util.extension.setInvisibility
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class AddTreeActivity : BindingActivity<ActivityAddTreeBinding>(R.layout.activity_add_tree) {
     private val addTreeViewModel by viewModels<AddTreeViewModel>()
+    private lateinit var ornamentView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,6 +30,7 @@ class AddTreeActivity : BindingActivity<ActivityAddTreeBinding>(R.layout.activit
         setOrnamentId()
         addListeners()
         initView()
+        collectData()
     }
 
     private fun setOrnamentId() {
@@ -31,10 +43,59 @@ class AddTreeActivity : BindingActivity<ActivityAddTreeBinding>(R.layout.activit
         }
     }
 
+    private fun collectData() {
+        addTreeViewModel.treeOverviewState.flowWithLifecycle(lifecycle).onEach { uiState ->
+            when (uiState) {
+                is UiState.Success -> {
+                    with(binding) {
+                        ivAddTreeLightBulb.setInvisibility(uiState.data.ornamentOverviewList[3])
+                        ivAddTreeBall1.setInvisibility(uiState.data.ornamentOverviewList[0])
+                        ivAddTreeHat.setInvisibility(uiState.data.ornamentOverviewList[1])
+                        ivAddTreeBall2.setInvisibility(uiState.data.ornamentOverviewList[2])
+                        ivAddTreeBall3.setInvisibility(uiState.data.ornamentOverviewList[4])
+                        ivAddTreeCookie.setInvisibility(uiState.data.ornamentOverviewList[5])
+                        ivAddTreeBall5.setInvisibility(uiState.data.ornamentOverviewList[6])
+                        ivAddTreeBall4.setInvisibility(uiState.data.ornamentOverviewList[7])
+                        ivAddTreeSocks.setInvisibility(uiState.data.ornamentOverviewList[8])
+                        ivAddTreeBall6.setInvisibility(uiState.data.ornamentOverviewList[9])
+                        ivAddTreeBall7.setInvisibility(uiState.data.ornamentOverviewList[10])
+                        ivAddTreeCandy.setInvisibility(uiState.data.ornamentOverviewList[11])
+                        ivAddTreeBall8.setInvisibility(uiState.data.ornamentOverviewList[12])
+                        ivAddTreeBall9.setInvisibility(uiState.data.ornamentOverviewList[13])
+                    }
+
+                    ornamentView = when (addTreeViewModel.ornamentId.value) {
+                        0 -> binding.ivAddTreeBall1
+                        1 -> binding.ivAddTreeHat
+                        2 -> binding.ivAddTreeBall2
+                        3 -> binding.ivAddTreeLightBulb
+                        4 -> binding.ivAddTreeBall3
+                        5 -> binding.ivAddTreeCookie
+                        6 -> binding.ivAddTreeBall5
+                        7 -> binding.ivAddTreeBall4
+                        8 -> binding.ivAddTreeSocks
+                        9 -> binding.ivAddTreeBall6
+                        10 -> binding.ivAddTreeBall7
+                        11 -> binding.ivAddTreeCandy
+                        12 -> binding.ivAddTreeBall8
+                        else -> binding.ivAddTreeBall9
+                    }
+
+                    ornamentView.setInvisibility(false)
+                }
+
+                else -> Unit
+            }
+        }.launchIn(lifecycleScope)
+    }
+
     private fun moveToTreeDetail() {
-        Intent(this@AddTreeActivity, TreeDetailActivity::class.java).apply {
-            // TODO 트리 값 다시 한 번 받아오기
-            startActivity(this)
+        addTreeViewModel.viewModelScope.launch {
+            Intent(this@AddTreeActivity, TreeDetailActivity::class.java).apply {
+                ornamentView.setInvisibility(true)
+                delay(1500)
+                startActivity(this)
+            }
         }
     }
 
